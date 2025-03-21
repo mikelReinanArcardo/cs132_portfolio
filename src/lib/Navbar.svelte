97a6b1619b5eff8a1efae4ef53c2aaaa3d011a7e
mikelReinanArcardo <mikeljuly28@gmail.com>
2025-03-22 00:00:24 +0800

    feat: add link to data set

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -51,7 +51,7 @@
 				</li>
 				<li>
 					<a
-						href="#"
+						href="https://docs.google.com/spreadsheets/d/1pk9NmED7tb6uZbccLIJrMk5TwXZ0mBls82uCE_opgkk/edit?usp=sharin"
 						class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
 						aria-current="page">Methods</a
 					>
