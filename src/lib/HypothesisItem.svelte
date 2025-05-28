52079a84295e48b72079014931ca376e99f1647d
mikelReinanArcardo <mikeljuly28@gmail.com>
2025-05-29 00:16:43 +0800

    fix: make padding smaller for mobile devices

--- src/lib/HypothesisItem.svelte
+++ src/lib/HypothesisItem.svelte
@@ -2,17 +2,17 @@
     let {number=0, question, alt, nul} = $props();
 </script>
 
-<section class="flex flex-col justify-center text-white gap-8 text-xl">
-    <div class="flex flex-col gap-2 p-12 bg-hpq rounded-xl shadow-sm shadow-lightblue">
+<section class="flex flex-col justify-center text-white gap-8 text-lg md:text-xl">
+    <div class="flex flex-col gap-2 p-8 md:p-12 bg-hpq rounded-xl shadow-sm shadow-lightblue">
             <p class="text-lightblue font-medium">
                 Q{number}:
             </p>
-            <p class="text-2xl">
+            <p class="text-xl md:text-2xl">
                 {question}
             </p>
     </div>
-    <div class="flex gap-12">
-        <div class="flex flex-col gap-4 p-8 bg-hpa rounded-xl shadow-md shawdow-hpq w-full">
+    <div class="flex gap-6 md:gap-12">
+        <div class="flex flex-col gap-4 p-4 md:p-8 bg-hpa rounded-xl shadow-md shawdow-hpq w-full">
             <p class="text-lightblue font-medium">
                 Alternative Hypothesis:
             </p>
@@ -20,7 +20,7 @@
                 {alt}
             </p>
         </div>
-        <div class="flex flex-col gap-4 p-8 bg-hpa rounded-xl shadow-md shawdow-hpq w-full">
+        <div class="flex flex-col gap-4 p-4 md:p-8 bg-hpa rounded-xl shadow-md shawdow-hpq w-full">
             <p class="text-lightblue font-medium">
                 Null Hypothesis:
             </p>
--- src/lib/ModelItem.svelte
+++ src/lib/ModelItem.svelte
@@ -2,9 +2,9 @@
     let {question, answer} = $props();
 </script>
 
-<section class="flex flex-col justify-center text-white gap-8 text-xl">
-    <div class="flex flex-col gap-2 p-12 bg-hpq rounded-xl shadow-sm shadow-lightblue">
-        <p class="text-lightblue text-2xl">
+<section class="flex flex-col justify-center text-white gap-8 text-lg md:text-xl">
+    <div class="flex flex-col gap-2 p-8 md:p-12 bg-hpq rounded-xl shadow-sm shadow-lightblue">
+        <p class="text-lightblue text-xl md:text-2xl">
             {question}
         </p>
         <p>
